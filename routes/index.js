29ebf5f34ed1aa8f1c9f8b77737b861acb90c04b
takumi2003 <takumi20030819@gmail.com>
2024-09-09 02:38:49 +0900

    9/9 認証エラーでストップ

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,45 @@
 const express = require('express');
 const router = express.Router();
+const mysql = require('mysql');
 
 let todos = [];
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { 
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'takumi0819',
+  database: 'todo_app'
+});
+
+// アプリケーションの起動時に一度だけ接続
+connection.connect((err) => {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+  console.log('connected as id ' + connection.threadId);
+});
+
+router.get('/', function (req, res, next) {
+  res.render('index', {
     title: 'ToDo App',
     todos: todos,
   });
 });
 
-router.post('/',function(req, res, next){
+router.post('/', function (req, res, next) {
   const todo = req.body.add;
-  todos.push(todo);
-  res.redirect('/');
+  connection.query(
+    `INSERT INTO tasks (user_id, content) VALUES (1, ?)`,
+    [todo],
+    (error, results) => {
+      if (error) {
+        console.error(error);
+        return res.status(500).send('Database query error');
+      }
+      res.redirect('/');
+    }
+  );
 });
 
 module.exports = router;
